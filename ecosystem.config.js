ca2829573cb0f76650c52145b7e6fed6f566746f
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-03-28 08:45:43 +0300

    update repo

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ path: '.env' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPO,
+  DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy':
+        'rm -rf frontend && cd backend && npm i &&  npm run build && pm2 restart all',
+    },
+  },
+};
